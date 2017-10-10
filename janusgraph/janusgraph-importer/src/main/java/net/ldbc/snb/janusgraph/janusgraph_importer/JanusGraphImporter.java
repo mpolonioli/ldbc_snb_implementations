59a71c5f03bb0541e0ed90f5083f177fd92bc87f
mpolonioli <m.polonioli@campus.unimib.it>
2017-10-10 17:03:50 +0200

    removed unused import

--- janusgraph/janusgraph-importer/src/main/java/net/ldbc/snb/janusgraph/janusgraph_importer/JanusGraphImporter.java
+++ janusgraph/janusgraph-importer/src/main/java/net/ldbc/snb/janusgraph/janusgraph_importer/JanusGraphImporter.java
@@ -26,7 +26,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.TimeZone;
-import java.util.concurrent.TimeUnit;
 
 public class JanusGraphImporter {
 	private static final Logger logger =
