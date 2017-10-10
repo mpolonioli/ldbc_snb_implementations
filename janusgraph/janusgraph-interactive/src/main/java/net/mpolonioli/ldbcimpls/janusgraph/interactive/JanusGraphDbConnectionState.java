6e4c3bd2d838c10d9b186e9893d40669b059a449
mpolonioli <m.polonioli@campus.unimib.it>
2017-10-10 23:51:46 +0200

    deleted unused import

--- janusgraph/janusgraph-interactive/src/main/java/net/mpolonioli/ldbcimpls/janusgraph/interactive/JanusGraphDbConnectionState.java
+++ janusgraph/janusgraph-interactive/src/main/java/net/mpolonioli/ldbcimpls/janusgraph/interactive/JanusGraphDbConnectionState.java
@@ -7,46 +7,30 @@ import com.ldbc.driver.DbConnectionState;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphFactory;
 
-import org.apache.commons.configuration.BaseConfiguration;
-
 import java.util.logging.Level;
 
 public class JanusGraphDbConnectionState extends DbConnectionState{
 
 	private final JanusGraph client;
 
-	  public JanusGraphDbConnectionState() {
-		  
-	    //create the connection specifing all the configurations
-	    /*
-	    BaseConfiguration config = new BaseConfiguration();
-	    config.setDelimiterParsingDisabled(true);
-	    config.setProperty("storage.backend", "hbase");
-	    config.setProperty("storage.hostname", "127.0.0.1");
-	    config.setProperty("storage.hbase.keyspace", "ldbc_snb_socialnet");
-	    config.setProperty("storage.hbase.table", "janusgraph");  
-	    config.setProperty("cache.db-cache", "true");
-	    config.setProperty("cache.db-cache-size", "0.5");
-	    config.setProperty("gremlin.graph", "org.janusgraph.core.JanusGraphFactory");
-	    client = JanusGraphFactory.open(config);
-	    */
-		  
-	    // create the connection via configuration file  
-	    client = JanusGraphFactory.open("//opt//janusgraph//janusgraph-0.1.1-hadoop2//conf//janusgraph-hbase.properties");
-	  }
+	public JanusGraphDbConnectionState() {
 
-	  public JanusGraph getClient() {
-	    return client;
-	  }
+		// create the connection via configuration file  
+		client = JanusGraphFactory.open("//opt//janusgraph//janusgraph-0.1.1-hadoop2//conf//janusgraph-hbase.properties");
+	}
 
-	  @Override
-	  public void close() throws IOException {
-	    try {
-	      client.close();
-	    } catch (Exception ex) {
-	      java.util.logging.Logger.getLogger(JanusGraphDb.class.getName())
-	          .log(Level.SEVERE, null, ex);
-	    }
-	  }
+	public JanusGraph getClient() {
+		return client;
+	}
+
+	@Override
+	public void close() throws IOException {
+		try {
+			client.close();
+		} catch (Exception ex) {
+			java.util.logging.Logger.getLogger(JanusGraphDb.class.getName())
+			.log(Level.SEVERE, null, ex);
+		}
+	}
 
 }
